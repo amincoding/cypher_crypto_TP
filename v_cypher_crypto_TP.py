d9fd2804bb75291b7454d38f664a06661edbc7d3
amincoding <aminabdedaiem@gmail.com>
2021-04-05 23:51:22 +0100

    amin

--- v_cypher_crypto_TP.py
+++ v_cypher_crypto_TP.py
@@ -2,10 +2,27 @@
 
 """
 
-author : amin abdedaiem
-date & time : april 5th 2021
-subject :  Vigenere Cipher cryptography 
-link : 
+    author : amin abdedaiem
+    date & time : april 5th 2021
+    subject :  Vigenere Cipher cryptography 
+    link : https://github.com/amincoding/cypher_crypto_TP
 
 
 """
+
+# creating the function that it does all the encryption then add to it a last step thought out a quick function
+def Tashfiiiir(text , cle):
+    pass
+
+# creating the function that it does all the decryption then add to it a last step through out a quick function
+def Break_Tashfiiiir(text , cle):
+    pass
+
+def main():
+    pass
+
+
+
+
+if __name__ == '__main__':
+    main()
