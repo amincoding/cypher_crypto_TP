5364f9bcfef608f07dbcb686f97ba9e1bb4e3ca8
amincoding <aminabdedaiem@gmail.com>
2021-04-06 00:44:31 +0100

    amin

--- v_cypher_crypto_TP.py
+++ v_cypher_crypto_TP.py
@@ -9,20 +9,31 @@
 
 
 """
+import json
 
-# creating the function that it does all the encryption then add to it a last step thought out a quick function
+
+path_to_json = "secret.json"
+
+with open(path_to_json, "r") as p:
+    info = json.load(p)
+
+Tashfiiiir_alphabits = info["alphabits"]
+key = info["key"]
+
+# creating the function that it does all the encryption then add to it a last step through out a quick function
 def Tashfiiiir(text , cle):
     pass
 
 # creating the function that it does all the decryption then add to it a last step through out a quick function
-def Break_Tashfiiiir(text , cle):
+def Break_Tashfiiiir(cypher , cle):
     pass
 
+# main execution function
 def main():
-    pass
+    
+    message = input("enter the message that u want to encrypt : ")
+    Tashfiiiir(message ,key)
 
-
-
-
+# main function
 if __name__ == '__main__':
     main()
